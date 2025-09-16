46010a9aaed9506df1bff7fb5739f034ed4320a4
Denny09310 <k.denny2000@gmail.com>
2025-09-16 08:26:08 +0200

    fix: missing docs link to component API

--- docs/LumexUI.Docs.Client/Common/Navigation/NavigationStore.cs
+++ docs/LumexUI.Docs.Client/Common/Navigation/NavigationStore.cs
@@ -72,6 +72,7 @@ public class NavigationStore
 			.Add( new( nameof( LumexDropdown ) ) )
 			.Add( new( nameof( LumexDropdownItem ) ) )
 			.Add( new( nameof( LumexDropdownMenu ) ) )
+			.Add( new( nameof( LumexKbd ) ) )
 			//.Add( nameof( LumexInputBase<T> ) )
 			//.Add( nameof( LumexInputFieldBase<T> ) )
 			.Add( new( nameof( LumexLink ) ) )
